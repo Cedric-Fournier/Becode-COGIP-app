4cd2b499e96613141465abcda1999bc2d13dbee1
wilfort <wilfort.stephane@hotmail.fr>
2018-08-28 15:14:48 +0200

    crud company

--- assets/php/CRUD/societe/read.php
+++ assets/php/CRUD/societe/read.php
@@ -1,10 +1,24 @@
 <?php
-$sql="SELECT * from company where id = 1";
-include '/var/www/html/COGIP-app/assets/php-pdo/connect.php';
-$requete->execute();
-$donnees = $requete->fetchAll();
-$requete->closeCursor();
-//print_r($donnees);
+// company
+$id=1;
+    $sql="SELECT * from company where company.id = $id";
+    include '/var/www/html/COGIP-app/assets/php-pdo/connect.php';
+    $requete->execute();
+    $company = $requete->fetchAll();
+    $requete->closeCursor();
+    $company=$company['0'];
+// person
+    $sql="SELECT lastname,firstname from person where person.company = $id";
+    include '/var/www/html/COGIP-app/assets/php-pdo/connect.php';
+    $requete->execute();
+    $person = $requete->fetchAll();
+    $requete->closeCursor();
+// bill
+    $sql="SELECT object from bill where bill.company = $id";
+    include '/var/www/html/COGIP-app/assets/php-pdo/connect.php';
+    $requete->execute();
+    $bill = $requete->fetchAll();
+    $requete->closeCursor();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,10 +34,32 @@ $requete->closeCursor();
     <header></header>
     <section>
         <div class="row">
-            <div class="col-2">désignation sociale de la société</div><div class="col-4">l'adresse de la société</div><div class="col-2">le pays</div><div class="col-2">le n° de TVA</div><div class="col-2">le n° de téléphone</div>
-            <?php foreach ($donnees as $key => $value) { ?>
-            <div class="col-2"><?=$value['name']?></div><div class="col-4"><?=($value['street']." ".$value['number'].", ".$value['zip']." ".$value['city'])?></div><div class="col-2"><?=$value['country']?></div><div class="col-2"><?=$value['VAT']?></div><div class="col-2"><?=$value['phone']?></div>
-            <?php } ?>
+            <div class="col-12 text-center title"><h1>désignation sociale de la société</h1></div>
+            <div class="col-12 text-center"><?=$company['name']?></div>
+            <div class="col-8 text-center"><h1>l'adresse de la société</h1></div>
+            <div class="col-4 text-center"><h1>le pays</h1></div>
+            <div class="col-8 text-center"><?=($company['street']." ".$company['number'].", ".$company['zip']." ".$company['city'])?></div>
+            <div class="col-4 text-center"><?=$company['country']?></div>
+            <div class="col-12 text-center"><h1>le n° de téléphone</h1></div>
+            <div class="col-12 text-center"><?=$company['phone']?></div>
+            <div class="col-12 text-center"><h1>le n° de TVA</h1></div>
+            <div class="col-12 text-center"><?=$company['VAT']?></div>
+            <div class="col-6 text-center"><h1>liste des personnes</h1></div>
+            <div class="col-6 text-center"><h1>liste des factures</h1></div>
+            <div class="col-6 text-center">
+                <ul>
+                    <?php foreach ($person as $key => $value) { ?>
+                        <li><?=($value['lastname']." ".$value['firstname'])?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+            <div class="col-6 text-center">
+                <ul>
+                    <?php foreach ($bill as $key => $value) { ?>
+                        <li><?=$value['object']?></li>
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
     </section>
     <footer></footer>
