7da5139496b38ff46faef8977499559fe54ece07
wilfort <wilfort.stephane@hotmail.fr>
2018-08-28 10:26:19 +0200

    struture de base

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../../vendor/components/font-awesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/components/font-awesome/css/fontawesome.min.css">
 </head>
 <body>
     <header></header>
     <section></section>
     <footer></footer>
-    <script src="../../../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
