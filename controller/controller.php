26f67bc6621485851d3087a1b2b7e71056da4d6e
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-08-30 16:08:37 +0200

    mvc de bill et billDetail fonctionnel

--- controller/controller.php
+++ controller/controller.php
@@ -5,14 +5,22 @@
         $reponse = getDirectory();
         require "views/directory.view.php";
         $reponse->closeCursor();
-        
     }
-
     function detailPersonPage(){
         require "models/person.model.php";
         $person = getDetailPerson();
         require "views/detailPerson.view.php";
         $reponse->closeCursor();
+    }
+    function billPage(){
+      require "models/model_bill.php";
+      require "views/view_bill.php";
+      $req->closeCursor();
+    }
+    function detailBillPage(){
+      require "models/model_billDetail.php";
+      require "views/view_billDetail.php";
+      $req->closeCursor();
     }
 
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
-
     switch ($_GET["page"]) {
         case 'directory':
-            require "controllers/controller.php";
+            require "controller/controller.php";
             directoryPage();
             break;
 
         case 'detailPerson':
-            require "controllers/controller.php";
+            require "controller/controller.php";
             detailPersonPage();
             break;
-                    
-        default:            
+
+        case 'bill':
+            require "controller/controller.php";
+            billPage();
+            break;
+
+        case 'detailbill':
+            require "controller/controller.php";
+            detailBillPage();
+            break;
+
+        default:
             echo "Home page";
             break;
     }
