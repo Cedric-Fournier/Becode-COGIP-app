3868fe1db20bf8d8788a87b5c2b5d4557401a561
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-08-31 16:29:06 +0200

    31/08 16:29

--- controller/controller.php
+++ controller/controller.php
@@ -5,13 +5,37 @@
         $reponse = getDirectory();
         require "views/directory.view.php";
         $reponse->closeCursor();
-        
     }
-
     function detailPersonPage(){
         require "models/person.model.php";
         $person = getDetailPerson();
         require "views/detailPerson.view.php";
+    }
+    function billPage(){
+        require "models/model_bill.php";
+        require "views/view_bill.php";
+        $req->closeCursor();
+    }
+    function detailBillPage(){
+        require "models/model_billDetail.php";
+        require "views/view_billDetail.php";
+        $req->closeCursor();
+    }
+
+    //// ADMIN ////
+    function addPersonAdminPage(){
+        require "models/person.model.php";
+        $person = getAddPerson();
+        $getCompany = getCompanyName();
+        require "views/admin/addPerson.view.php";
+    }
+
+    function updatePersonAdminPage(){
+        require "models/person.model.php";
+        $personUp = getUpdatePerson();
+        $getPerson = getUpdateDetailPerson();
+        $getCompany = getCompanyName();
+        require "views/admin/updatePerson.view.php";
     }
 
 ?>
