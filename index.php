9a44113710f7908826988457189bc534ae6951d8
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-09-03 12:05:50 +0200

    supressions redondance

--- index.php
+++ index.php
@@ -116,14 +116,3 @@ switch ($_GET["page"]) {
         break;
 }
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>COGIPapp</title>
-    </head>
-    <body>
-    </body>
-</html>
