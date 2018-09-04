97ab9fb2df55cad4b2cb331a2d136acbc4342b12
wilfort <wilfort.stephane@hotmail.fr>
2018-09-04 17:12:13 +0200

    fin travail

--- assets/config/php/config.php
+++ assets/config/php/config.php
@@ -4,10 +4,10 @@
         $db_config = array();
         $db_config['SGBD']	= 'mysql';
         $db_config['HOST']	= 'localhost';
-        $db_config['DB_NAME']	= 'id6613249_cogipapp';//id6613249_cogipapp
+        $db_config['DB_NAME']	= 'cogipapp';//id6613249_cogipapp
         $db_config['CHARSET'] = 'utf8';
-        $db_config['USER']	= 'rootid6613249_root';//rootid6613249_root
-        $db_config['PASSWORD']	= 'rootroot';//rootroot
+        $db_config['USER']	= 'root';//rootid6613249_root
+        $db_config['PASSWORD']	= 'PRli1992';//rootroot
         $db_config['OPTIONS']	= array(
             // Activation des exceptions PDO :
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
