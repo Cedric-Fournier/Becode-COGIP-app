17f27237b47ddb04d1217cb0f81bcf450f0b3efc
wilfort <wilfort.stephane@hotmail.fr>
2018-09-04 17:03:52 +0200

    fin travail

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,5 +2,6 @@
             © COGIP</div></footer>
         <script src="vendor/components/jquery/jquery.min.js"></script>
         <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="snippet.js"></script>
     </body>
 </html>
